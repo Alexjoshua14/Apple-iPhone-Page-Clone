2bef7b9eead85344c4ee8e660906c152db92710b
Alex <ajoshua14@icloud.com>
2023-06-24 15:46:37 -0700

    Tweak Buy Now section copy sizing

--- sections/buyNow.tsx
+++ sections/buyNow.tsx
@@ -3,9 +3,12 @@ import Buy from '../components/Buy';
 const BuyNow = () => {
   return (
     <div className="h-[14rem] flex justify-center">
-      <div className="flex flex-col justify-center items-center gap-4 h-full w-1/2">
-        <span className="font-medium text-3xl">iPhone 14 and iPhone 14 Plus</span>
-        <span className="font-medium text-md">From $799 or $33.29/mo. for 24mo. before trade-in*</span>
+      <div className="flex flex-col justify-center items-center gap-4 h-full text-center max-w-[280px] md:max-w-none">
+        <span className="font-medium text-xl md:text-3xl">iPhone 14 and iPhone 14 Plus</span>
+        <div className="flex flex-wrap items-center justify-center gap-1 font-medium text-sm md:text-md md:px-0">
+          <span>From $799 or $33.29/mo. for 24mo. </span>
+          <span> before trade-in*</span>
+        </div>
         <Buy size={1.5} />
       </div>
     </div>
