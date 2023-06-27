40032e1c11a915e810053900fdf10f3f50597cdf
Alex <ajoshua14@icloud.com>
2023-06-27 12:08:53 -0700

    Base grid layout established

--- sections/specGallery.tsx
+++ sections/specGallery.tsx
@@ -1,8 +1,47 @@
 
 const SpecGallery = () => {
   return (
-    <div className="h-[50rem] border-2 border-yellow-500">
-      Specs Section
+    <div className="h-max md:h-[100rem] border-2 border-yellow-500">
+      <div className="flex flex-col md:grid grid-cols-3 md:grid-rows-10 gap-2 h-full">
+        <div className="h-[31.75rem] order-1 md:h-full col-span-2 row-span-2 border-2 border-teal-500">
+
+        </div>
+        <div className="h-[23.5rem] order-2 md:h-full border-2 border-teal-500">
+
+        </div>
+        <div className="h-[23.5rem] order-3 md:h-full border-2 border-teal-500">
+
+        </div>
+
+        <div className="h-[23.5rem] order-4 md:h-full border-2 border-blue-500">
+
+        </div>
+        <div className="h-[23.5rem] order-5 md:h-full col-span-2 border-2 border-pink-500">
+
+        </div>
+        <div className="h-[31.75rem] order-6 md:order-7 md:h-full md:col-span-2 row-span-2 border-2 border-yellow-500">
+
+        </div>
+        <div className="h-[23.5rem] order-7 md:order-6 md:h-full border-2 border-orange-500">
+
+        </div>
+        <div className="h-[23.5rem] order-8 md:h-full border-2 border-green-500">
+
+        </div>
+        <div className="h-[23.5rem] order-9 md:h-full col-span-2 border-2 border-teal-500">
+
+        </div>
+        <div className="h-[23.5rem] order-10 md:h-full border-2 border-teal-500">
+
+        </div>
+
+        <div className="h-[31.75rem] order-11 col-span-2 row-span-3 border-2 border-teal-500">
+
+        </div>
+        <div className="h-[31.75rem] order-12 row-span-3 border-2 border-teal-500">
+
+        </div>
+      </div>
     </div>
   );
 }
